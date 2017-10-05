5ddc1fdce497990552941aca9f27d31b21315335
Pierre-Chanel Gauthier <pierrechanel.gauthier@gmail.com>
2017-10-05 21:06:59 +0200

    Create all templates and web controllers

---
+++ src/Controller/RulesController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Rule\Ability;
+use App\Entity\Rule\Attribute;
+use App\Entity\Rule\Burst;
+use App\Entity\Rule\CanonicalStuff;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RulesController extends AbstractController
+{
+    /**
+     * @Route("/règles", name="rules")
+     */
+    public function indexAction()
+    {
+        return $this->render('pages/rules/index.html.twig');
+    }
+
+    /**
+     * @Route("/règles/capacités-arcaniques", name="rules.abilities")
+     */
+    public function abilitiesAction()
+    {
+        $repo = $this->getDoctrine()->getRepository(Ability::class);
+
+        return $this->render('pages/rules/abilities.html.twig', [
+            'abilities' => $repo->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/règles/attributs-physiques", name="rules.attributes")
+     */
+    public function attributesAction()
+    {
+        $repo = $this->getDoctrine()->getRepository(Attribute::class);
+
+        return $this->render('pages/rules/attributes.html.twig', [
+            'attributes' => $repo->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/règles/décharges-d-arcanite", name="rules.bursts")
+     */
+    public function burstsAction()
+    {
+        $repo = $this->getDoctrine()->getRepository(Burst::class);
+
+        return $this->render('pages/rules/bursts.html.twig', [
+            'bursts' => $repo->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/règles/équipements", name="rules.stuff")
+     */
+    public function stuffAction()
+    {
+        $repo = $this->getDoctrine()->getRepository(CanonicalStuff::class);
+
+        return $this->render('pages/rules/stuff.html.twig', [
+            'weapons' => $repo->findAllWeapons(),
+            'armors' => $repo->findAllArmors(),
+            'objects' => $repo->findAllObjects(),
+            'expendables' => $repo->findAllExpendables(),
+        ]);
+    }
+}
---
+++ src/Controller/UniverseController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Rule\Arcane;
+use App\Entity\Rule\Deity;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UniverseController extends AbstractController
+{
+    /**
+     * @Route("/univers", name="universe")
+     */
+    public function indexAction()
+    {
+        return $this->render('pages/rules/index.html.twig');
+    }
+
+    /**
+     * @Route("/univers/arcanes", name="universe.arcanes")
+     */
+    public function arcanesAction()
+    {
+        $repo = $this->getDoctrine()->getRepository(Arcane::class);
+
+        return $this->render('pages/universe/arcanes.html.twig', [
+            'arcanes' => $repo->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/univers/divinités", name="universe.deities")
+     */
+    public function deitiesAction()
+    {
+        $repo = $this->getDoctrine()->getRepository(Deity::class);
+
+        return $this->render('pages/universe/bursts.html.twig', [
+            'deities' => $repo->findAll(),
+        ]);
+    }
+}
